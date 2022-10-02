6769bb9d8a19ae78de64a9388a17a7d0499c6be1
litwak913 <litwak913@qq.com>
2022-10-02 19:54:06 +0800

    wip:custom footer links

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -6,21 +6,7 @@ use Html;
 class PageHooks {
     public static function customFooter( Skin $skin, string $key, array &$footerlinks  ) {
         if ( $key === 'places' ) {
-        //echo var_dump($footerLinks);
-            $footerlinks['git'] = Html::element( 'a',
-                [
-                    'href' => 'https://github.com/mcbbs-wiki',
-                    'rel' => 'noreferrer noopener'
-                ],
-            'Github'
-            );
-            $footerlinks['status'] = Html::element( 'a',
-                [
-                    'href' => 'https://stats.uptimerobot.com/DZLkwIoKBB',
-                    'rel' => 'noreferrer noopener'
-                ],
-            '服务状态'
-            );
+            //WIP
         };
         if ( $key === 'info' ) {
             $footerlinks['hello'] = $skin->msg( 'footerinfo' );
