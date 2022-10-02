1efa3dab4152814384f53969570a84ddf95bb7a4
litwak913 <litwak913@qq.com>
2022-10-02 20:14:31 +0800

    remove custom links

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -5,9 +5,6 @@ use Skin;
 use Html;
 class PageHooks {
     public static function customFooter( Skin $skin, string $key, array &$footerlinks  ) {
-        if ( $key === 'places' ) {
-            //WIP
-        };
         if ( $key === 'info' ) {
             $footerlinks['hello'] = $skin->msg( 'footerinfo' );
         };
