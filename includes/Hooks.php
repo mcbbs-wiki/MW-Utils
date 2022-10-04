0e0a7736b642d5e15c1ac5866466975714b06035
litwak913 <litwak913@qq.com>
2022-10-04 17:10:05 +0800

    Tag

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -2,12 +2,16 @@
 namespace MCBBSWiki;
 
 use Skin;
-use Html;
+use Parser;
+use Tags;
 class PageHooks {
     public static function customFooter( Skin $skin, string $key, array &$footerlinks  ) {
         if ( $key === 'info' ) {
-            $footerlinks['hello'] = $skin->msg( 'footerinfo' );
+            $footerlinks['hello'] = $skin->msg( 'footerinfo' )->parse();
         };
+    }
+    public static function onParserFirstCallInit( Parser $parser ) {
+        $parser->setHook('mcbbs-avatar',[Tags::class,'renderTagMCBBSAvatar']);
     }
 }
 
---
+++ includes/Tags.php
@@ -0,0 +1,10 @@
+<?php
+namespace MCBBSWiki;
+
+use Parser;
+use PPFrame;
+class Tags {
+    public static function renderTagMCBBSAvatar($input,array $args,Parser $parser,PPFrame $frame){
+        return 'HELLO WORLD';
+    }
+}
