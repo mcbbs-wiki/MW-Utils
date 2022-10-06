13df9d43acdcc759952b0a426b0f271bdb378be8
litwak913 <litwak913@qq.com>
2022-10-06 11:31:32 +0800

    format

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -1,22 +1,25 @@
 <?php
+
 namespace MCBBSWiki;
 
 use Skin;
 use Parser;
 use MCBBSWiki\Tags;
 
-class PageHooks {
-    public static function customFooter( Skin $skin, string $key, array &$footerlinks  ) {
-        if ( $key === 'info' ) {
-            $msg=$skin->msg( 'footerinfo' );
-            if(!$msg->isDisabled()){
+class PageHooks
+{
+    public static function customFooter(Skin $skin, string $key, array &$footerlinks)
+    {
+        if ($key === 'info') {
+            $msg = $skin->msg('footerinfo');
+            if (!$msg->isDisabled()) {
                 $footerlinks['footerinfo'] = $msg->parse();
             }
         };
     }
-    public static function onParserFirstCallInit( Parser $parser ) {
-        $parser->setHook('mcbbs-avatar',[Tags::class,'renderTagMCBBSAvatar']);
-        $parser->setHook('bilibili',[Tags::class,'renderTagBilibili']);
+    public static function onParserFirstCallInit(Parser $parser)
+    {
+        $parser->setHook('mcbbs-avatar', [Tags::class, 'renderTagMCBBSAvatar']);
+        $parser->setHook('bilibili', [Tags::class, 'renderTagBilibili']);
     }
 }
-
--- includes/Tags.php
+++ includes/Tags.php
@@ -1,34 +1,39 @@
 <?php
+
 namespace MCBBSWiki;
 
 use Parser;
 use PPFrame;
 use Html;
 
-class Tags {
-    public static function renderTagMCBBSAvatar($input,array $args,Parser $parser,PPFrame $frame){
-        $uid=isset($args['uid'])?htmlspecialchars( $args['uid'] ):'1';
-        $image = Html::element('img',[
-            'src'=>"https://www.mcbbs.net/uc_server/avatar.php?uid=$uid&size=big",
-            'class'=>"mcbbs-avatar mcbbs-avatar-$uid",
-            'style'=>'width:200px;height:200px;'],'');
+class Tags
+{
+    public static function renderTagMCBBSAvatar($input, array $args, Parser $parser, PPFrame $frame)
+    {
+        $uid = isset($args['uid']) ? htmlspecialchars($args['uid']) : '1';
+        $image = Html::element('img', [
+            'src' => "https://www.mcbbs.net/uc_server/avatar.php?uid=$uid&size=big",
+            'class' => "mcbbs-avatar mcbbs-avatar-$uid",
+            'style' => 'width:200px;height:200px;'
+        ], '');
         return $image;
     }
-    public static function renderTagBilibili($input,array $args,Parser $parser,PPFrame $frame) {
+    public static function renderTagBilibili($input, array $args, Parser $parser, PPFrame $frame)
+    {
         $attr = [
-            'allowfullscreen'=>'allowfullscreen',
-            'frameborder'=>'0',
-            'sandbox'=>'allow-top-navigation allow-same-origin allow-forms allow-scripts',
-            'scrolling'=>'no',
-            'width'=>isset($args['width'])?$args['width']:'600',
-            'height'=>isset($args['height'])?$args['height']:'600'
+            'allowfullscreen' => 'allowfullscreen',
+            'frameborder' => '0',
+            'sandbox' => 'allow-top-navigation allow-same-origin allow-forms allow-scripts',
+            'scrolling' => 'no',
+            'width' => isset($args['width']) ? $args['width'] : '600',
+            'height' => isset($args['height']) ? $args['height'] : '600'
         ];
-        if(isset($args['bv'])){
-            $src='https://player.bilibili.com/player.html?bvid='.htmlspecialchars($args['bv']).'&high_quality=1';
-            $attr['src']=$src;
-            return Html::element('iframe',$attr,'');
+        if (isset($args['bv'])) {
+            $src = 'https://player.bilibili.com/player.html?bvid=' . htmlspecialchars($args['bv']) . '&high_quality=1';
+            $attr['src'] = $src;
+            return Html::element('iframe', $attr, '');
         } else {
-            return Html::element('p',['style'=>'color:red;font-size:160%'],wfMessage('bilibili-nobvid')->text());
+            return Html::element('p', ['style' => 'color:red;font-size:160%'], wfMessage('bilibili-nobvid')->text());
         }
     }
 }
