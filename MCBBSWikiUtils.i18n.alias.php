7f0a12c55814a4762b9851703a5c89dd06414de6
litwak913 <litwak913@qq.com>
2022-10-12 19:27:28 +0800

    add alias

---
+++ MCBBSWikiUtils.i18n.alias.php
@@ -0,0 +1,10 @@
+<?php
+$specialPageAliases = [];
+
+$specialPageAliases['en'] = [
+    'MCBBSCredit' => ['MCBBSCredit', 'MCBBS Credit']
+];
+
+$specialPageAliases['zh-hans'] = [
+    'MCBBSCredit' => ['MCBBS积分分析']
+];
