e7df83e7142f4c298abfa831959b6c7772922236
litwak913 <litwak913@qq.com>
2023-03-17 23:13:00 +0800

    extimg

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -9,13 +9,31 @@ use MediaWiki\Hook\SkinAddFooterLinksHook;
 use Parser;
 use PPFrame;
 use Skin;
-use SpecialPage;
 
 class Hooks implements ParserFirstCallInitHook, SkinAddFooterLinksHook {
-	private $ucenter;
-
+	private string $ucenter;
+	private $whitelistDomains;
+	private function checkDomain( string $url ) {
+		$domain = parse_url( $url, PHP_URL_HOST );
+	
+		// Check if we match the domain exactly
+		if ( in_array( $domain, $this->whitelistDomains ) )
+			return true;
+	
+		$valid = false;
+	
+		foreach( $this->whitelistDomains as $whitelistDomain ) {
+			$whitelistDomain = '.' . $whitelistDomain; // Prevent things like 'evilsitetime.com'
+			if( strpos( $domain, $whitelistDomain ) === ( strlen( $domain ) - strlen( $whitelistDomain ) ) ) {
+				$valid = true;
+				break;
+			}
+		}
+		return $valid;
+	}
 	public function __construct( ConfigFactory $configFactory ) {
 		$config = $configFactory->makeConfig( 'MCBBSWikiUtils' );
+		$this->whitelistDomains = $config->get("ExtImgWhiteList");
 		$this->ucenter = $config->get( 'UCenterURL' );
 	}
 
@@ -32,8 +50,27 @@ class Hooks implements ParserFirstCallInitHook, SkinAddFooterLinksHook {
 		$parser->setHook( 'ucenter-avatar', [ $this,'renderTagUCenterAvatar' ] );
 		$parser->setHook( 'mcbbs-credit',  [ $this,'renderTagMCBBSCredit' ] );
 		$parser->setHook( 'bilibili',  [ $this,'renderTagBilibili' ] );
+		$parser->setHook( 'ext-img',  [ $this,'renderTagExtimg' ] );
 	}
-
+	public function renderTagExtimg( $input, array $args, Parser $parser, PPFrame $frame ) {
+		if(!isset($args['src'])){
+			return '';
+		}
+		if(!$this->checkDomain($args['src'])){
+			return Html::element( 'p',
+			[ 'style' => 'color:#d33;font-size:larger;font-weight: bold;' ],
+			 wfMessage( 'extimg-invalidurl' )->text() );
+		}
+		$image = Html::element( 'img', [
+			'src' => $args['src'],
+			'title' => $args['title'] ?? null,
+			'alt' => $args['alt'] ?? null,
+			'width'=>$args['width'] ?? null,
+			'height'=>$args['height'] ?? null,
+			'class' => "ext-img"
+		], '' );
+		return $image;
+	}
 	public function renderTagUCenterAvatar( $input, array $args, Parser $parser, PPFrame $frame ) {
 		$parser->getOutput()->addModuleStyles( [ 'ext.mcbbswikiutils.avatar' ] );
 		if ( isset( $args['mili'] ) ) {
@@ -42,7 +79,7 @@ class Hooks implements ParserFirstCallInitHook, SkinAddFooterLinksHook {
 		$ucenter = $this->ucenter;
 		if ( empty( $ucenter ) ) {
 			return Html::element( 'p',
-			[ 'style' => 'color:#d33;font-size:160%;font-weight:bold' ],
+			[ 'style' => 'color:#d33;font-size:larger;font-weight:bold;' ],
 			 wfMessage( 'ucenteravatar-noucenterurl' )->text() );
 		}
 		$uid = isset( $args['uid'] ) ? htmlspecialchars( $args['uid'] ) : '1';
@@ -83,7 +120,7 @@ class Hooks implements ParserFirstCallInitHook, SkinAddFooterLinksHook {
 			return Html::rawElement( 'div', [ 'class' => "bilibili-video bilibili-video-$bvid" ], $video );
 		} else {
 			return Html::element( 'p',
-			[ 'style' => 'color:#d33;font-size:160%;font-weight:bold' ],
+			[ 'style' => 'color:#d33;font-size:larger;font-weight:bold;' ],
 			wfMessage( 'bilibili-nobvid' )->text() );
 		}
 	}
