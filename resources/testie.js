4f2bc7b74326ee62663e097511adaa03f9a90c62
litwak913 <litwak913@qq.com>
2023-07-14 15:18:30 +0800

    fix

--- resources/testie.js
+++ resources/testie.js
@@ -2,6 +2,6 @@ if ( $.client.profile().name === 'msie' ) {
 	const $warntext = $.parseHTML( mw.message( 'iewarning' ).parse() );
 	const $warn = $( '<div>' ).attr( 'class', 'ie-warning' );
 	$warn.append( $warntext );
-	$( '#firstHeading' ).append( $warn );
+	$( '#firstHeading' ).after( $warn );
 	mw.track( 'bbswiki.ieuser' );
 }
